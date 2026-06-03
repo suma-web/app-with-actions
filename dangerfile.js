63d233bd43a461d8edb706935eed25a670b415e3
sample tarou <sample@example.com>
2026-06-03 20:33:25 +0900

    dangerfile.jsの作成と実行設定

---
+++ dangerfile.js
@@ -0,0 +1,18 @@
+const { warn, danger } = require("danger");
+
+const changedLines =
+  danger.github.pr.additions +
+  danger.github.pr.deletions;
+
+if (changedLines > 200) {
+  warn("変更行数が200行を超えています");
+}
+
+const changedFiles =
+  danger.git.modified_files.length +
+  danger.git.created_files.length +
+  danger.git.deleted_files.length;
+
+if (changedFiles > 10) {
+  warn("編集ファイル数が10を超えています");
+}
